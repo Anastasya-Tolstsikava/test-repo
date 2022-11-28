257022da18d3cb156a07f92d8a033a7b82db4b79
Anastasia Tolstikova <60227191+Anastasya-Tolstsikava@users.noreply.github.com>
2022-11-28 18:15:22 +0300

    Update auto-check-changed-files.sh

--- .github/workflows/auto-check-changed-files.sh
+++ .github/workflows/auto-check-changed-files.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-echo "run-sh: Inside auto-check-changed-files.sh"
+CHANGED=$(git diff origin/main...HEAD --name-only)
+CHANGELOG_IN_MASTER=$(git show origin/main:CHANGELOG.md)
+THIRD_ROW=$(head -3 CHANGELOG.md | tail -1)
+THIRD_ROW_IN_MASTER=$(echo "${CHANGELOG_IN_MASTER}" | head -3 | tail -1)
 
-# print content of json holding all files to be checked
-a=$(cat /home/runner/files.json)
-echo "Content of files.json: $a" 
+if [[ "$CHANGED" == *"CHANGELOG.md"* ]]; then
+  echo "${THIRD_ROW}"
+  echo "${THIRD_ROW_IN_MASTER}"
+  LAST_VERSION_IN_MASTER=${THIRD_ROW_IN_MASTER#* }
+  LAST_VERSION_IN_MASTER=${LAST_VERSION_IN_MASTER% *}
 
-# get number of files to check
-num_files=$(jq '. | length' /home/runner/files.json)
-echo "number of files to check = $num_files"
+  NEW_VERSION_FROM_BRANCH=${THIRD_ROW#* }
+  NEW_VERSION_FROM_BRANCH=${NEW_VERSION_FROM_BRANCH% *}
 
-# loop over each file to be checked
-for file in $(jq  '.[]' /home/runner/files.json | cut -d '"' -f 2); do
-    echo "checking file: $file"
-
-done
+  echo "${LAST_VERSION_IN_MASTER}"
+  echo "${NEW_VERSION_FROM_BRANCH}"
+  if [[ NEW_VERSION_FROM_BRANCH > LAST_VERSION_IN_MASTER ]]; then
+      # doesn't work for versions because it is not the number. Requires more complicated comparing logic.
+      echo "Great! version number is bigger than in master."
+  else
+    echo "New version number in CHANGELOG.md file need to be grater than the latest in master branch."
+    exit 1
+  fi
+else
+  echo "CHANGELOG.md file need to be updated."
+  exit 1
+fi
+#echo "${CHANGED}"
+#echo "${CHANGELOG_IN_MASTER}"
+#echo "${THIRD_ROW}"
+exit 0
